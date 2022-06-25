b60413ee8f0689d3c12c5af9ca44196a73d88e08
dhythm <yuta.okada.20@gmail.com>
2022-06-25 20:30:49 +0900

    update index page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,13 +1,18 @@
 import { Grid, GridItem } from "@chakra-ui/react"
+import { LoginForm } from "app/core/components"
 import Layout from "app/core/layouts/Layout"
-import { BlitzPage } from "blitz"
+import { BlitzPage, useSession } from "blitz"
+import { Suspense } from "react"
 
 /*
  * This file is just for a pleasant getting started page for your new app.
  * You can delete everything in here and start from scratch if you like.
  */
 
-const Home: BlitzPage = () => {
+const LoginOrTop = () => {
+  const session = useSession()
+  console.log({ session })
+  if (!session.userId) return <LoginForm />
   return (
     <Grid h={"200px"} templateColumns={"repeat(5, 1fr)"} templateRows={"repeat(2, 1fr)"} gap={4}>
       <GridItem rowSpan={2} colSpan={1} bg={"blue.500"} />
@@ -17,6 +22,14 @@ const Home: BlitzPage = () => {
       <GridItem colSpan={1} bg={"blue.500"} />
       <GridItem colSpan={4} bg={"blue.500"} />
     </Grid>
+  )
+}
+
+const Home: BlitzPage = () => {
+  return (
+    <Suspense fallback="Loading...">
+      <LoginOrTop />
+    </Suspense>
   )
 }
 
