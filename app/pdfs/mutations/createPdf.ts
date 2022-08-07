0872833335236e2c8c773c1f708c37c11a665cfc
dhythm <yuta.okada.20@gmail.com>
2022-08-07 21:33:52 +0900

    update

--- app/pdfs/mutations/createPdf.ts
+++ app/pdfs/mutations/createPdf.ts
@@ -1,5 +1,6 @@
 import ReactPDF from "@react-pdf/renderer"
 import { SampleDocument } from "app/core/components"
+import { Base64Decode } from "base64-stream"
 import { resolver } from "blitz"
 import { promises as fs } from "fs"
 import { z } from "zod"
@@ -15,16 +16,24 @@ export default resolver.pipe(
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     // const pdf = await db.pdf.create({ data: input })
     const fileName = `${__dirname}/example.pdf`
-    ReactPDF.render(SampleDocument(), fileName)
+    await ReactPDF.render(SampleDocument(), fileName)
 
     const stream = await ReactPDF.renderToStream(SampleDocument())
-    const string = await ReactPDF.renderToString(SampleDocument())
+    // const string = await ReactPDF.renderToString(SampleDocument())
 
-    let buffer: Buffer = await fs.readFile(fileName)
+    // NOTE: base64str does NOT match with the encoded PDF
+    const buffer = await fs.readFile(fileName)
+    const arrayBuffer = new ArrayBuffer(buffer.length)
+    const view = new Uint8Array(arrayBuffer)
+    for (let i = 0; i < buffer.length; i++) {
+      view[i] = buffer[i]!
+    }
     const base64str = Buffer.from(
-      new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ""),
+      new Uint8Array(arrayBuffer).reduce((data, byte) => data + String.fromCharCode(byte), ""),
       "binary"
     ).toString("base64")
+
+    stream.pipe(new Base64Decode())
 
     console.log({ fileName })
     console.log({ base64str })
