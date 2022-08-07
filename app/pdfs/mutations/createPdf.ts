c8dc0756645d74ff1dd87a60799c88711bce01a2
dhythm <yuta.okada.20@gmail.com>
2022-08-07 21:04:30 +0900

    wip

--- app/pdfs/mutations/createPdf.ts
+++ app/pdfs/mutations/createPdf.ts
@@ -1,6 +1,7 @@
 import ReactPDF from "@react-pdf/renderer"
 import { SampleDocument } from "app/core/components"
 import { resolver } from "blitz"
+import { promises as fs } from "fs"
 import { z } from "zod"
 
 const CreatePdf = z.object({
@@ -13,9 +14,22 @@ export default resolver.pipe(
   async (input) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     // const pdf = await db.pdf.create({ data: input })
-    ReactPDF.render(SampleDocument(), `${__dirname}/example.pdf`)
-    console.log(__dirname)
-    const pdf = null
+    const fileName = `${__dirname}/example.pdf`
+    ReactPDF.render(SampleDocument(), fileName)
+
+    const stream = await ReactPDF.renderToStream(SampleDocument())
+    const string = await ReactPDF.renderToString(SampleDocument())
+
+    let buffer: Buffer = await fs.readFile(fileName)
+    const base64str = Buffer.from(
+      new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ""),
+      "binary"
+    ).toString("base64")
+
+    console.log({ fileName })
+    console.log({ base64str })
+
+    const pdf = base64str
 
     return pdf
   }
