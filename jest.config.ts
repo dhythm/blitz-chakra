ac9f75651b13d317ee86a5fc3efd72a5c449f595
dhythm <yuta.okada.20@gmail.com>
2022-08-14 00:43:20 +0900

    wip

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,16 @@
 import type { Config } from "@jest/types"
+import { readFileSync } from "fs"
+
+// Reading the SWC compilation config and remove the "exclude"
+// for the test files to be compiled by SWC
+const { exclude: _, ...swcJestConfig } = JSON.parse(readFileSync(`${__dirname}/.swcrc`, "utf-8"))
 
 const config: Config.InitialOptions = {
   preset: "blitz",
+  transform: {
+    "^.+\\.[tj]sx?$": ["@swc/jest", swcJestConfig],
+  },
+  transformIgnorePatterns: ["/node_modules/**/.+\\.(t|j)sx?$"],
 }
 
 export default config
